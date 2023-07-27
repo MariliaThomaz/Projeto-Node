093f89ac10eaf69485d3df721b8c2223412f220b
MariliaThomaz <marilia.silva9@fatec.sp.gov.br>
2023-07-26 21:55:48 -0300

    segundo commit

--- app.js
+++ app.js
@@ -1,31 +1,81 @@
-const express    = require('express');
-const app        = express();
-const db         = require('./DB/connection');
-const bodyParser = require('body-parser');
+const express    = require('express');//
+const exphbs     = require('express-handlebars');
+const app        = express();//
+const path       = require('path');//
+const db         = require('./DB/connection');//
+const bodyParser = require('body-parser');//
+const Job        = require('./models/Job');
+const Sequelize  = require('sequelize');
+const Op         = Sequelize.Op;
 
-const PORT = 3000;
+const PORT = 3000;//
 
 app.listen(PORT, function() {
-    console.log("O Express está rodando na  porta: ", PORT);
-});
+    console.log(`O Express está rodando na porta ${PORT}`);
+  });//
 //tem usar  o boby parser
-app.use(bodyParser.urlencoded({ extended: false}));
+// body parser
+app.use(bodyParser.urlencoded({ extended: false }));
 
+//handle bars
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));//falando qual é o pricipal
+app.set('view engine', 'handlebars');
+
+
+//static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // DB  Conexão
 db
 .authenticate()
-.then(()=> { // faz teste  para cada ves que sitema  sera  inciado
-    console.log("Conectpi ao bamcp com sucesso");
+.then(() => {// faz teste  para cada ves que sitema  sera  inciado
+  console.log("Conectou ao banco com sucesso");
 })
 .catch(err => {
-    console.log("Ocorreu um erro ao conecta", err);
+  console.log("Ocorreu um erro ao conectar", err);
 });
 
+
 //rota
-app.get('/', (req, res)=> {
-    res.send("Esta funcionado 4");
-});
+// routes
+app.get('/', (req, res) => {
+
+    let search = req.query.job;
+    let query  = '%'+search+'%'; // PH -> PHP, Word -> Wordpress, press -> Wordpress
+  
+    if(!search) {
+      Job.findAll({order: [
+        ['createdAt', 'DESC']
+      ]})
+      .then(jobs => {
+    
+        res.render('index', {
+          jobs
+        });
+    
+      })
+      .catch(err => console.log(err));
+    } else {
+      Job.findAll({
+        where: {title: {[Op.like]: query}},
+        order: [
+          ['createdAt', 'DESC']
+      ]})
+      .then(jobs => {
+        console.log(search);
+        console.log(search);
+    
+        res.render('index', {
+          jobs, search
+        });
+    
+      })
+      .catch(err => console.log(err));
+    }
+  
+    
+  });
 
 //definido as rotas de  maneria  unica
 app.use('/jobs', require('./routes/jobs'));// estou consequindo acendo as rotas dentro daquele arquivos
--- routes/jobs.js
+++ routes/jobs.js
@@ -2,17 +2,35 @@
 // que  ira  fazer ira  fazer  Sequelize com o banco
 
 const express = require('express');// ira tomar conta das rotas
-const router = express.Router();//objeto do espress é este Router
-const Job = require('../models/Job');//chamando o  model
+const router  = express.Router();//objeto do espress é este Router
+const Job     = require('../models/Job');//chamando o  model
 
 // faz  um  teste
-router.get('/test', (req, res)=>{
+router.get('/test', (req, res) => {
     res.send('deu certo');
-});
+  });
 
 
 
 //quera  priema  rota que  é POST 
+
+// detalhe da vaga -> view/1, view/2
+router.get('/view/:id', (req, res) => Job.findOne({
+    where: {id: req.params.id}
+  }).then(job => {
+  
+    res.render('view', {
+      job
+    });
+  
+  }).catch(err => console.log(err)));
+  
+// form da rota de envio
+router.get('/add', (req, res) => {
+    res.render('add');
+  })
+  
+
 
 //add o JOB vida POST
 router.post('/add', (req, res)=> {
